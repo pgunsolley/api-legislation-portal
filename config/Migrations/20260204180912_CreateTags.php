1dfdcb44b05446fdda73056a5a718510a62d02a6
Patrick Gunsolley <patrick.gunsolley@outlook.com>
2026-02-04 11:20:28 -0700

    Added ddev config; added users and tags migrations

---
+++ config/Migrations/20260204180912_CreateTags.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\BaseMigration;
+
+class CreateTags extends BaseMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/migrations/4/en/migrations.html#the-change-method
+     *
+     * @return void
+     */
+    public function change(): void
+    {
+        $this
+            ->table('tags')
+            ->addColumn('user_id', 'uuid', [
+                'default' => null,
+                'null' => false,
+            ])
+            ->addColumn('body', 'string', [
+                'default' => null,
+                'null' => false,
+            ])
+            ->addColumn('bill_id', 'integer', [
+                'default' => null,
+                'null' => false,
+                'signed' => false,
+            ])
+            ->create();
+    }
+}
